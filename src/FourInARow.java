c9a7149dc8b4d91f6a583d3c3686eed8cab88852
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-01-19 19:53:37 -0500

    Computer Evaluation implemented Does it work? probably not.. Should it work? In my mind, yes.

--- src/FourInARow.java
+++ src/FourInARow.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Random;
 
 /**
  * TicTacToe class implements the interface
@@ -18,12 +17,14 @@ public class FourInARow implements IGame {
 
     public static int COLOR_PLAYER = RED, COLOR_COMPUTER = BLUE;
 
+    private final Random random;
 
     /**
      * clear board and set current player (???)
      */
     public FourInARow() {
         clearBoard();
+        random = new Random();
     }
 
     @Override
@@ -60,21 +61,83 @@ public class FourInARow implements IGame {
         }
 
         //Check if any will lose
-        for(Integer move : validMoves) {
-            if(checkMoveResult(COLOR_PLAYER,move) != PLAYING) {
+        for (Integer move : validMoves) {
+            if (checkMoveResult(COLOR_PLAYER, move) != PLAYING) {
                 return move;
             }
         }
 
-        return 0;
+        List<Integer> bestMoves = new ArrayList<>();
+        int bestEval = 0;
+
+        for (Integer move : validMoves) {
+            int eval = evaluatePosition(move, COLOR_COMPUTER);
+            if (eval > bestEval) {
+                bestEval = eval;
+                bestMoves.clear();
+                bestMoves.add(move);
+            } else if (eval == bestEval) {
+                bestMoves.add(move);
+            }
+        }
+
+        return bestMoves.get(random.nextInt(bestMoves.size()));
     }
 
     public int getLocation(int location) {
-        return board[location / COLS][location % COLS];
+        return inBounds(location) ? board[location / COLS][location % COLS] : -1;
+    }
+
+    private int evaluatePosition(int location, int player) {
+
+        if (getLocation(location) != EMPTY) {
+            return -1;
+        }
+
+        int eval = 0;
+
+        for (int d : new int[]{1, COLS, COLS + 1}) {
+            //Check the range
+            int length = 0, count = 0;
+            boolean goPos = true, goNeg = true;
+            for (int i = 0; i < 4; i++) {
+                int probePos = getLocation(location + d * i);
+                int probeNeg = getLocation(location - d * i);
+
+                if (goPos && (probePos == player || probePos == 0)) {
+                    length++;
+                    if (probePos == player) {
+                        count++;
+                    }
+                } else {
+                    goPos = false;
+                }
+
+                if (goNeg && (probeNeg == player || probeNeg == 0)) {
+                    length++;
+                    if (probeNeg == player) {
+                        count++;
+                    }
+                } else {
+                    goNeg = false;
+                }
+            }
+            if (length >= 3) {
+                eval += count;
+            }
+        }
+
+        return eval;
+    }
+
+    private boolean inBounds(int location) {
+        return location >= 0 && location < ROWS * COLS;
     }
 
     public void setLocation(int location, int value) {
-        board[location / COLS][location % COLS] = value;
+        if (inBounds(location)) {
+            board[location / COLS][location % COLS] = value;
+        }
     }
 
     /**
