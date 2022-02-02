308600968e5e76ff0cfa7bb39644a235bf4ac2e8
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-02 15:47:18 -0500

    Renamed InstructionsActivity.java

--- app/src/main/java/edu/quinnipiac/ser210/fourinarow/InstructionsActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/fourinarow/InstructionsActivity.java
@@ -3,11 +3,11 @@ package edu.quinnipiac.ser210.fourinarow;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class InstructionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_instructions);
     }
 }
