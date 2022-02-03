a25bed24838b2a803c76579cdea01049103a0544
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-03 12:50:31 -0500

    Started thinking about how things will be organized

--- app/src/main/java/edu/quinnipiac/ser210/fourinarow/activities/NameInputActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/fourinarow/activities/NameInputActivity.java
@@ -1,16 +1,21 @@
 package edu.quinnipiac.ser210.fourinarow.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import edu.quinnipiac.ser210.fourinarow.R;
 
-public class InstructionsActivity extends AppCompatActivity {
+public class NameInputActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_instructions);
+        setContentView(R.layout.activity_name_input);
+    }
+
+    public void onStart(View view) {
+
     }
 }
