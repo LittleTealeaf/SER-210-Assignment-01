0ba9faee606365f93f020532bdba0da977112146
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-03 21:41:27 -0500

    Misc Cleanup

--- app/src/main/java/edu/quinnipiac/ser210/fourinarow/activities/GreeterActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/fourinarow/activities/GreeterActivity.java
@@ -10,17 +10,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import edu.quinnipiac.ser210.fourinarow.R;
 
+/**
+ * @author Thomas Kwashnak
+ */
 public class GreeterActivity extends AppCompatActivity {
 
     public static final String KEY_NAME;
-
-
 
     static {
         KEY_NAME = "name";
     }
 
-    EditText inputName;
+    private EditText inputName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +30,26 @@ public class GreeterActivity extends AppCompatActivity {
 
         inputName = (EditText) findViewById(R.id.greeter_input_name);
 
-        if(savedInstanceState != null) {
+        if (savedInstanceState != null) {
             inputName.setText(savedInstanceState.getString(KEY_NAME));
         }
     }
 
     public void onClickBegin(View view) {
         String name = inputName.getText().toString();
-        Intent intent = new Intent(this,GameActivity.class);
-        intent.putExtra(KEY_NAME,name);
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra(KEY_NAME, name);
         startActivity(intent);
     }
 
     public void onClickInstructions(View view) {
-        Intent intent = new Intent(this,InstructionsActivity.class);
+        Intent intent = new Intent(this, InstructionsActivity.class);
         startActivity(intent);
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putString(KEY_NAME,inputName.getText().toString());
+        savedInstanceState.putString(KEY_NAME, inputName.getText().toString());
     }
 }
